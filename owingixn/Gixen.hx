23e50bc63f215cf8bd2944a067afc0c8b847afab
tecteun <tec@tecteun.homelinux.org>
2016-03-04 12:01:09 +0100

    iss tests with owin https://coding.abel.nu/2014/05/whats-this-owin-stuff-about/

---
+++ owingixn/Gixen.hx
@@ -0,0 +1,35 @@
+typedef AppFunc = cs.system.Func_2<cs.system.collections.generic.IDictionary_2<String, Dynamic>, cs.system.threading.tasks.Task>;
+
+/*
+    https://coding.abel.nu/2014/05/whats-this-owin-stuff-about/
+    https://github.com/AndersAbel/OwinSelfHostTest/blob/master/OwinSelfHostExpanded/Handler.cs
+ */
+class Gixen {
+    
+    static function main() 
+	{
+        
+        //owin.IAppBuilder
+        microsoft.owin.hosting.WebApp.Start("http://localhost:4242", Startup);
+        cs.system.Console.ReadLine();
+        
+    }
+    
+    private static function Startup(appBuilder:owin.IAppBuilder):Void {
+        var func:cs.system.Func_2<AppFunc, AppFunc> = BasicAuthenticationMiddleware;
+        appBuilder.Use(func, new cs.NativeArray(0)); 
+    }
+    
+    private static function BasicAuthenticationMiddleware(next:AppFunc):AppFunc
+    {
+       return function(context:cs.system.collections.generic.IDictionary_2<String, Dynamic>){ 
+           trace(context.get_Item("owin.RequestPath"));
+           var writer = new cs.system.io.StreamWriter(cast(context.get_Item("owin.ResponseBody"), cs.system.io.Stream));
+           writer.Write("aap");
+           writer.Close();
+           cs.system.threading.Thread.Sleep(100);
+           return new cs.system.threading.tasks.Task(cast next.Invoke(context));
+       };
+    }
+    
+}
