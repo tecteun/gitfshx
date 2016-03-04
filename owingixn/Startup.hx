b89bfdff31e7b87627c7a44837dc5b0542298223
tecteun <tec@tecteun.homelinux.org>
2016-03-04 14:35:06 +0100

    update

---
+++ owingixn/Startup.hx
@@ -0,0 +1,39 @@
+typedef AppFunc = cs.system.Func_2<cs.system.collections.generic.IDictionary_2<String, Dynamic>, cs.system.threading.tasks.Task>;
+
+/*
+    https://coding.abel.nu/2014/05/whats-this-owin-stuff-about/
+    https://github.com/AndersAbel/OwinSelfHostTest/blob/master/OwinSelfHostExpanded/Handler.cs
+ */
+class Startup {
+    
+    static function main() 
+	{
+        
+        //owin.IAppBuilder
+        //microsoft.owin.hosting.WebApp.Start("http://localhost:4242", Configuration);
+        //cs.system.Console.ReadLine();
+        
+    }
+    
+    @:final
+    public function Configuration(appBuilder:owin.IAppBuilder):Void {
+        
+        var func:cs.system.Func_2<AppFunc, AppFunc> = BasicAuthenticationMiddleware;
+        appBuilder.Use(func, new cs.NativeArray(0)); 
+        
+        microsoft.owin.extensions.IntegratedPipelineExtensions.UseStageMarker(appBuilder, owin.PipelineStage.MapHandler);
+    }
+    
+    private function BasicAuthenticationMiddleware(next:AppFunc):AppFunc
+    {
+       return function(context:cs.system.collections.generic.IDictionary_2<String, Dynamic>){ 
+           trace(context.get_Item("owin.RequestPath"));
+           var writer = new cs.system.io.StreamWriter(cast(context.get_Item("owin.ResponseBody"), cs.system.io.Stream));
+           writer.Write("aap");
+           writer.Close();
+           cs.system.threading.Thread.Sleep(100);
+           return new cs.system.threading.tasks.Task(cast next.Invoke(context));
+       };
+    }
+    
+}
