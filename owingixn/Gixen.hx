ff877fae459a7160caa1da229e7ae9ddd6c82691
tecteun <tec@tecteun.homelinux.org>
2016-03-08 10:21:13 +0100

    update iis

--- owingixn/Gixen.hx
+++ owingixn/Gixen.hx
@@ -1,35 +1,39 @@
 typedef AppFunc = cs.system.Func_2<cs.system.collections.generic.IDictionary_2<String, Dynamic>, cs.system.threading.tasks.Task>;
 
 /*
+    https://github.com/james-world/owinmultihosting
+    https://github.com/HaxeFoundation/HaxeManual/wiki/Haxe-C%23
     https://coding.abel.nu/2014/05/whats-this-owin-stuff-about/
     https://github.com/AndersAbel/OwinSelfHostTest/blob/master/OwinSelfHostExpanded/Handler.cs
  */
-@:classCode("[assembly: OwinStartup(typeof(Startup))]\n")
-class Gixen {
+@:classCode("[assembly: OwinStartup(typeof(Gixen))]\n") //does not work, needs to be *Above* class
+class Gixen { //optionally use class: Startup, to use owin default Startup.Configuration bootstrap
     
     static function main() 
 	{
-        
-        //owin.IAppBuilder
-        microsoft.owin.hosting.WebApp.Start("http://localhost:4242", Startup);
+        microsoft.owin.hosting.WebApp.Start("http://localhost:4242", Configuration);
         cs.system.Console.ReadLine();
-        
     }
     
-    private static function Startup(appBuilder:owin.IAppBuilder):Void {
+    @:final
+    public static function Configuration(appBuilder:owin.IAppBuilder):Void {
+        
         var func:cs.system.Func_2<AppFunc, AppFunc> = BasicAuthenticationMiddleware;
         appBuilder.Use(func, new cs.NativeArray(0)); 
+        
+        microsoft.owin.extensions.IntegratedPipelineExtensions.UseStageMarker(appBuilder, owin.PipelineStage.MapHandler);
     }
     
     private static function BasicAuthenticationMiddleware(next:AppFunc):AppFunc
     {
+        
        return function(context:cs.system.collections.generic.IDictionary_2<String, Dynamic>){ 
            trace(context.get_Item("owin.RequestPath"));
            var writer = new cs.system.io.StreamWriter(cast(context.get_Item("owin.ResponseBody"), cs.system.io.Stream));
            writer.Write("aap");
            writer.Close();
-           cs.system.threading.Thread.Sleep(100);
-           return new cs.system.threading.tasks.Task(cast next.Invoke(context));
+           cs.system.threading.Thread.Sleep(1000);
+           return null;//new cs.system.threading.tasks.Task(cast next.Invoke(context));
        };
     }
     
