a7ec30dd80ca59fcd4578876fd8fc0d7d97755d3
tecteun <tec@tecteun.homelinux.org>
2016-03-10 11:38:03 +0100

    no message

--- owingixn/Gixen.hx
+++ owingixn/Gixen.hx
@@ -11,20 +11,27 @@ typedef AppFunc = cs.system.Func_2<cs.system.collections.generic.IDictionary_2<S
 @:classCode("[assembly: OwinStartup(typeof(Gixen))]\n") //does not work, needs to be *Above* class
 class Gixen { //optionally use class: Startup, to use owin default Startup.Configuration bootstrap
     static var fp:sys.io.FileOutput;
+    private static var taskFactory:cs.system.threading.tasks.TaskFactory = null;
     static function main() 
 	{
+        init();
         microsoft.owin.hosting.WebApp.Start("http://localhost:4242", Configuration);
         cs.system.Console.ReadLine();
 
     }
     
+    private static function init(){
+        taskFactory = new cs.system.threading.tasks.TaskFactory();
+    }
+    
     private static function log(s:String){
-        fp.writeString(s);
+        fp.writeString(s + "\n\r");
         fp.flush();
     }
     
     @:final
     public static function Configuration(appBuilder:owin.IAppBuilder):Void {
+        init();
         fp = sys.io.File.append(cs.system.AppDomain.CurrentDomain.SetupInformation.ApplicationBase + "/log.txt", false);
         log('Configuration-> start ${cs.system.AppDomain.CurrentDomain.SetupInformation.ApplicationName}');
         
@@ -35,23 +42,29 @@ class Gixen { //optionally use class: Startup, to use owin default Startup.Confi
     
     private static function BasicMiddleware(next:AppFunc):AppFunc
     {
-        cs.system.threading.Thread.Sleep(1000);
-        
         return function(context:cs.system.collections.generic.IDictionary_2<String, Dynamic>):cs.system.threading.tasks.Task{ 
+            var date = Date.now().getTime();
             log(context.get_Item("owin.RequestPath"));
             var stream = cast(context.get_Item("owin.ResponseBody"), cs.system.io.Stream);
             var headers:cs.system.collections.generic.IDictionary_2<String, cs.NativeArray<String>> = cast context.get_Item("owin.ResponseHeaders");
             headers.set_Item("Content-Length", cs.NativeArray.make(Std.string(0)));
 
-            var buffer:cs.NativeArray<cs.StdTypes.UInt8> = cs.system.text.Encoding.UTF8.GetBytes("Hello world!");
+            var buffer:cs.NativeArray<cs.StdTypes.UInt8> = cs.system.text.Encoding.UTF8.GetBytes("Hello world! " + context.get_Item("owin.RequestPath") + " " + date);
             context.set_Item("owin.ResponseStatusCode", 200);
             headers.set_Item("Content-Length", cs.NativeArray.make(Std.string(buffer.Length)));
             headers.set_Item("Content-Type", cs.NativeArray.make("text/plain"));
             
-            var tf = new cs.system.threading.tasks.TaskFactory();
-            //using native haxe this gives: ambigous overload, possibly due to types of stream.BeginWrite
-            return untyped __cs__("tf.FromAsync(stream.BeginWrite, stream.EndWrite, buffer, 0, buffer.Length, null, System.Threading.Tasks.TaskCreationOptions.AttachedToParent);");
-
+            var func = function(){ 
+                //do som async
+                cs.system.threading.Thread.Sleep(1000);
+                stream.Write(buffer, 0, buffer.Length);
+                stream.Flush();
+                stream.Close();
+                next.Invoke(context);
+            };
+            
+            //var task:cs.system.threading.tasks.Task = untyped __cs__("tf.FromAsync(stream.BeginWrite, stream.EndWrite, buffer, 0, buffer.Length, null, System.Threading.Tasks.TaskCreationOptions.AttachedToParent);");
+            return taskFactory.StartNew( new cs.system.Action(func));
        };
     }
     
